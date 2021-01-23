ea75177a2d6fa3f8306abafd8eb0325fdf641ad7
NStilesMM <nicholas.mcdonnell@gmail.com>
2021-01-23 15:05:45 -0500

    fixed algorithims format

--- bst_search.js
+++ bst_search.js
@@ -1,15 +1,16 @@
-function TreeNode(val, left, right) {
-  this.val = val === undefined ? 0 : val;
-  this.left = left === undefined ? null : left;
-  this.right = right === undefined ? null : right;
-}
-
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
 /**
  * @param {TreeNode} root
  * @param {number} val
  * @return {TreeNode}
  */
-
 var searchBST = function (root, val) {
   if (root.val === val) {
     return root;
@@ -21,26 +22,23 @@ var searchBST = function (root, val) {
     let t1 = queue.shift();
     let t2 = queue.shift();
 
-    if (t1.val === val) {
-      let answer = t1;
-      break;
+    if (t1) {
+      if (t1.val === val) {
+        return t1;
+      } else {
+        queue.push(t1.left);
+        queue.push(t1.right);
+      }
+      ("");
     }
-    if (t2.val === val) {
-      let answer = t2;
-      break;
+    if (t2) {
+      if (t2.val === val) {
+        return t2;
+      } else {
+        queue.push(t2.left);
+        queue.push(t2.right);
+      }
     }
-
-    if ((!t1.left && !t1.right) || (!t2.left && !t2.right)) {
-      return null;
-    }
-    queue.push(t1.left);
-    queue.push(t1.right);
-    queue.push(t2.left);
-    queue.push(t2.right);
   }
-  return answer;
+  return null;
 };
-
-let r = [4, 2, 7, 1, 3];
-let v = 2;
-console.log(searchBST(r, v));
